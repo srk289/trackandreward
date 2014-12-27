44737c70197a29c7f5e6dcfcaa1736a77c584316
Srikanth Kondaparthy <srk289@gmail.com>
2014-12-27 23:17:50 +1100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name trackandrewardApp
+ * @description
+ * # trackandrewardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('trackandrewardApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackandrewardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trackandrewardApp
+ */
+angular.module('trackandrewardApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
